c8c52f75004fd9643b5b6d9f646d9e298c021d28
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-02-15 21:36:33 -0500

    Se crea el tipo de dato para la respuesta del search

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gif.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,7 @@ export class GifsService {
   private apiKey = 'cNfvwsbH6YWWSkkcTfJWQKBh1kFuhUv2';
   private _historial: string[] = [];
   
-  // TODO: Cambiar any por su tipo
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial(): string[] {
     return [...this._historial];
@@ -27,8 +27,8 @@ export class GifsService {
     this._historial.unshift(query);
     this._historial = this._historial.splice(0, 10);
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=cNfvwsbH6YWWSkkcTfJWQKBh1kFuhUv2&q=${ query }&limit=10`)
-      .subscribe((resp: any) => {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=cNfvwsbH6YWWSkkcTfJWQKBh1kFuhUv2&q=${ query }&limit=10`)
+      .subscribe((resp) => {
         this.resultados = resp.data;
         console.log(resp.data);
       });
