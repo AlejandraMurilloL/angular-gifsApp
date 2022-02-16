0f70529ae8cabc3510984e6c75ff2c40d5aed302
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-02-15 21:12:54 -0500

    Se consume api get gifs por query y se muestran los gifs en cards

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -7,6 +7,9 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   private apiKey = 'cNfvwsbH6YWWSkkcTfJWQKBh1kFuhUv2';
   private _historial: string[] = [];
+  
+  // TODO: Cambiar any por su tipo
+  public resultados: any[] = [];
 
   get historial(): string[] {
     return [...this._historial];
@@ -24,8 +27,9 @@ export class GifsService {
     this._historial.unshift(query);
     this._historial = this._historial.splice(0, 10);
 
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=cNfvwsbH6YWWSkkcTfJWQKBh1kFuhUv2&q=Harry Potter&limit=10')
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=cNfvwsbH6YWWSkkcTfJWQKBh1kFuhUv2&q=${ query }&limit=10`)
       .subscribe((resp: any) => {
+        this.resultados = resp.data;
         console.log(resp.data);
       });
   }
